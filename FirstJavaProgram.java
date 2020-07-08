60107f9359d8026d9677f9b1289eafadab50c3f8
Jeremy Akatsa <51394348+jeyakatsa@users.noreply.github.com>
2020-07-07 19:25:23 -0700

    Delete FirstJavaProgram.java

--- FirstJavaProgram.java
+++
@@ -1,7 +0,0 @@
-public class FirstJavaProgram {
-    public static void main(String[] args) {
-        System.out.println(
-        "My name is Jeremy Akatsa. I am 32 years old. My hometown is San Jose, CA"
-        );
-    }
-}
