5fcabc185b1be44597aa6057ab95fd9c016a8489
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-07-09 21:34:46 -0700

    First & Second Commit

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println(
+        "My name is Jeremy Akatsa. I am 32 years old. My hometown is San Jose, CA"
+        );
+    }
+}
