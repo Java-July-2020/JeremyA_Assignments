11c6af1dd5ca803c9e8d3398680eec3c1d30cf73
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-07-12 16:21:52 -0700

    Hashmatique Done.

--- 01-java-fundamentals/07-map-of-the-hashmatique/MapOfTheHash.java
+++ 01-java-fundamentals/07-map-of-the-hashmatique/MapOfTheHash.java
@@ -7,9 +7,9 @@ public class MapOfTheHash{
         trackList.put("Diamonds", "Find light in the beautiful sea, I choose to be happy.");
         trackList.put("Glow In The Dark", "The stars are flickering, the sky is closing in, but I won't let go.");
         trackList.put("Experience As One", "Let's go on a mystical journey, to experience as one.");
-
-        for (HashMap.Entry<String, String> entry : trackList.entrySet()){
-            System.out.println("Track: " + entry.getKey() + " Lyrics: " + entry.getValue());
+        
+        for (String key : map.keySet()) {
+            System.out.println(String.format("Track: %s: Lyrics: %s", key, map.get(key)));
         }
     }
 }
