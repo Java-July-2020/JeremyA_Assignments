4d824c2d75770232664eb260386aac4eb905cc79
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-08-16 17:00:22 -0700

    Until Tomorrow.

--- 03-java-spring/04-login-and-registration/02-events-belt-reviewer/src/main/java/com/jeremyakatsa/eventsbelt/repositories/UserRepository.java
+++ 03-java-spring/04-login-and-registration/02-events-belt-reviewer/src/main/java/com/jeremyakatsa/eventsbelt/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.jeremyakatsa.eventsbelt.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.jeremyakatsa.eventsbelt.models.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
+    List<User> findAll();
 }
