8a6ff6301f56d9dca66422367ea2521642b3ad5d
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-08-17 18:55:33 -0700

    Tomorrow's another day.

--- 03-java-spring/04-login-and-registration/02-events-belt-reviewer/src/main/java/com/jeremyakatsa/eventsbelt/models/Message.java
+++ 03-java-spring/04-login-and-registration/02-events-belt-reviewer/src/main/java/com/jeremyakatsa/eventsbelt/models/Message.java
@@ -1,5 +1,58 @@
 package com.jeremyakatsa.eventsbelt.models;
 
-public class Message {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="messages")
+public class Message {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String comment;
+	@Column(updatable=false)
+	private Date createdAt;
+	private Date updatedAt;
+	@ManyToMany(fetch=FetchType.LAZY)
+	@JoinTable(name="users",
+		joinColumns=@JoinColumn(name="user_id"))
+	
+	public Message() {
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
 }
+
